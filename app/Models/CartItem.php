0da9615d64605e01179ed4a260efb57d46fbc5d5
Johnson Cheng <clare8151214@gmail.com>
2021-04-19 23:18:38 +0800

    ORM

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class CartItem extends Model
 {
     use HasFactory;
+    protected $guarded = [''];
+    protected $hidden = ['created_at'];
+    protected $appends = ['current_price'];
+
+    public function getCurrentPriceAttribute(){
+        return $this->quantity * 10;
+    }
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+    public function cart(){
+        return $this->belongsTo(Cart::class);
+    }
 }
